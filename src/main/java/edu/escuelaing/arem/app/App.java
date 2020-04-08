0a54121f8b85be7421e360dbc3535452e02d5019
jimmy chirivi <jimmychirivi09@gmail.com>
2020-04-08 15:44:40 -0500

    certificado configurado

--- src/main/java/edu/escuelaing/arem/app/App.java
+++ src/main/java/edu/escuelaing/arem/app/App.java
@@ -6,7 +6,10 @@ public class App {
 	public static void main(String[] args) {
 		port(4567);
 		
-		secure("/arquitectura-segura/ecikeystore.p12","jimmy970126",null,null);
+		secure("/Users/jimmy97/Desktop/arquitectura-segura/ecikeystore.p12","jimmy970126",null,null);
+		
+		
+		
 		get("/hello", (req, res) -> "Hello World");
 	}
 
